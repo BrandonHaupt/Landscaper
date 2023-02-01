36df572bfc09314a023e0bba970033e679c06e3e
Brandon Haupt <haupt.brandon@gmail.com>
2023-01-31 20:50:59 -0600

    Update 1.2
    - added the ability to check money, never said it the money actually worked but you can see the message

--- landscaper.py
+++ landscaper.py
@@ -1,11 +1,14 @@
 while(True):
     user_choice = input("[1] Mow Lawn [2] Check Stats [Q] Quit")
     money = 0
+    win_amount = 10
     
     if(user_choice == "1"):
         print("You found an lawn and asked the owner to mow it, you recieved $1 as payment, he found it kind of awkward that you used your teeth though")
         money += 1
     
+    if(user_choice == "2"):
+        print(f"Your current money is {money}")
     
     if(user_choice == "Q"):
         break
